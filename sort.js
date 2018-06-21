f9a2335461c9aa1976268fa118c4cb96dba40a6d
Essence Alexander <upperlefty@zoho.eu>
2018-06-21 00:17:22 -0700

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,12 +1,19 @@
 // [5, 1, 12, -5, 16, 2, 12, 14]
+//Index = Indx
 
 function selectionSort(arr){
-var minIndex, temp,
+var minIndx, temp,
 len = arr.length;
   for(var i = 0; i < len; i++){
-    minIndex = i;
+    minIndx = i;
 for(var  j = i+1; j<len; j++){
-       if(arr[j]<arr[minIdx]){
-          minIdx = j;
+       if(arr[j]<arr[minIndx]){
+          minIndx = j;
        }
     }
+temp = arr[i];
+    arr[i] = array[minIndx];
+    arr[minIndx] = temp;
+  }
+  return arr;
+}
