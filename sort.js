0794cd298d538f4fcec47a8ade5905cf86ff0ae0
Essence Alexander <upperlefty@zoho.eu>
2018-06-21 00:14:31 -0700

    Update sort.js

--- sort.js
+++ sort.js
@@ -5,3 +5,8 @@ var minIndex, temp,
 len = arr.length;
   for(var i = 0; i < len; i++){
     minIndex = i;
+for(var  j = i+1; j<len; j++){
+       if(arr[j]<arr[minIdx]){
+          minIdx = j;
+       }
+    }
